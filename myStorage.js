ff50a579cfd456ffe8d5647c77c87564cba1a6ad
Gustavo <gustavomorentin@Gustavos-MacBook-Air.local>
2018-08-28 09:14:43 -0500

    #2 Add method of delete

--- myStorage.js
+++ myStorage.js
@@ -2,10 +2,12 @@ const argv = require('yargs');
 
 // My method
 const getList = require('./core/getList');
+const deleteObject = require('./core/deleteObject');
 
 argv
 	.usage('\nWelcome Google Storage.\n\nUsage: $0 [options]')
 	.command( getList )
+	.command(deleteObject)
 	// -Here other command
 	.demandCommand()
 	.example('node myStorage.js ls my-project-inegi')
