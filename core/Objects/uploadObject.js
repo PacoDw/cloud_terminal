1e3e7541c991c163d023eb49f0605eec35f12f28
Paco Preciado Mendoza <__pm@outlook.com>
2018-09-03 16:57:49 -0500

    :pencil2: Issue #3 : Rename all variables

--- core/Objects/uploadObject.js
+++ core/Objects/uploadObject.js
@@ -3,21 +3,18 @@ const storage = require('../googleStorage');
 // This is the method to get list from the Google Storage
 module.exports = {
 
-    command: 'uploadObject [parameter] [filename]',
+    command: 'uploadObject [Bucketname] [filename]',
     aliases: ['up', 'upload'],
     describe: 'Upload an object from your bucket',
-    builder: yargs => yargs.default('parameter', 'filename'),
+    builder: yargs => yargs.default('nameBucket', 'my-project-inegi'),
     handler: argv => {
 
         storage
-            .bucket(argv.parameter)
+            .bucket(argv.Bucketname)
             .upload(argv.filename)
             .then(() => {
-                console.log(`${argv.filename} uploaded to ${argv.parameter}.`);
+                console.log(`${argv.filename} uploaded to ${argv.Bucketname}.`);
             })
-            .catch(err => {
-                console.error('ERROR:', err);
-            });
-
+            .catch(err => console.error('ERROR:',err.errors[0].message) );
     }
 }
