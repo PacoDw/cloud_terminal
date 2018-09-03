a2ad11eac168896df1135ccd6547660b90312ddc
Paco Preciado Mendoza <__pm@outlook.com>
2018-09-03 17:33:09 -0500

    :sparkles: Issue #12 : Add a middleware to handle the config file

--- myStorage.js
+++ myStorage.js
@@ -1,29 +1,35 @@
 const argv = require('yargs');
 
 // My method
-const getList = require('./core/Objects/getList');
+const listObject = require('./core/Objects/listObject');
 const deleteObject = require('./core/Objects/deleteObject');
 const downloadObject = require('./core/Objects/downloadObject');
 const uploadObject = require('./core/Objects/uploadObject');
 const renameObject = require('./core/Objects/renameObject');
 const createBucket = require('./core/Buckets/createBucket');
 const listBuckets = require('./core/Buckets/listBuckets');
+const localFolder = require('./core/localFolder');
 
+// Config
+const { config, checkPath } = require('./core/config');
 
 argv
+	.middleware( [ checkPath ] )
 	.usage('\nWelcome Google Storage.\n\nUsage: $0 [options]')
-	.command( getList )
-	.command(deleteObject)
-	.command(downloadObject)
-	.command(uploadObject)
-	.command(createBucket)
-	.command(listBuckets)
-	.command(renameObject)
-
+	.command( localFolder )
+	.command( uploadObject )
+	.command( createBucket )
+	.command( listObject )
+	.command( deleteObject )
+	.command( downloadObject )
+	.command( listBuckets )
+	.command( renameObject )
+	
 	// -Here other command
 	.demandCommand()
 	.example('node myStorage.js ls my-project-inegi')
 	.help()
 	.alias('help', 'h')
-	.wrap(72)
+	.config( config )
+	.wrap( argv.myWrap )
 	.argv;
