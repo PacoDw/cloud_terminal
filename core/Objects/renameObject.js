8b5191534ee1eac0f561b8eafb1bb6e9e748e35a
Paco Preciado Mendoza <__pm@outlook.com>
2018-09-03 17:07:37 -0500

    :pencil2: Issue #7 : Rename all variables

--- core/Objects/renameObject.js
+++ core/Objects/renameObject.js
@@ -2,19 +2,19 @@ const storage = require('../googleStorage')
 
 // This is the method to get list from the Google Storage
 module.exports = {
-  command: 'renameObject [parameter] [srcFilename] [destFilename]',
+  command: 'renameObject [nameBucket] [srcFilename] [destFilename]',
   aliases: ['ren', 'rename'],
   describe: 'Rename an object of your bucket',
-  builder: yargs => yargs.default('parameter', 'srcFilename', 'destFilename'),
+  builder: yargs => yargs.default('nameBucket', 'srcFilename', 'destFilename'),
   handler: argv => {
 
     storage
-      .bucket(argv.parameter)
-      .file(argv.srcFilename)
-      .move(argv.destFilename)
+      .bucket( argv.nameBucket )
+      .file( argv.srcFilename )
+      .move( argv.destFilename )
       .then(() => {
         console.log(
-          `gs://${argv.parameter}/${argv.srcFilename} renamed to gs://${argv.parameter}/${argv.destFilename}.`
+          `gs://${argv.nameBucket}/${argv.srcFilename} renamed to gs://${argv.nameBucket}/${argv.destFilename}.`
         )
       })
       .catch(err => {
