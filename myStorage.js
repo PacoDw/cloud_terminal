0ff26a3fed5be6ad4c2f05f97bd8da5d6ece8f0e
Paco Preciado Mendoza <__pm@outlook.com>
2018-09-21 16:51:16 -0500

    :sparkles: The commands were modulated in the index.js file : issue: #16

--- myStorage.js
+++ myStorage.js
@@ -1,40 +1,30 @@
 const yargs = require('yargs');
+const SettingsFile = require('./core/config');
 
-// Config
-const JsonFile = require('./core/config');
-const jsonFile = new JsonFile(yargs);
+const cmd = require('./core');
 
+let checkConfig =  _ => {
+	return { 
+		yargs :  SettingsFile.checkConfig(yargs)
+	} 
+}
 
-// My method
-const listObject = require('./core/Objects/listObject');
-const deleteObject = require('./core/Objects/deleteObject');
-const downloadObject = require('./core/Objects/downloadObject');
-const uploadObject = require('./core/Objects/uploadObject');
-const renameObject = require('./core/Objects/renameObject');
-const createBucket = require('./core/Buckets/createBucket');
-const listBuckets = require('./core/Buckets/listBuckets');
-const localFolder = require('./core/localFolder');
-
-
-let checkConfig = _ => jsonFile.checkConfig()
-
+/* All commands */
 yargs
 	.middleware( [checkConfig] )
 	.usage('\nWelcome Google Storage.\n\nUsage: $0 [options]')
-	.command( localFolder )
-	.command( uploadObject )
-	.command( createBucket )
-	.command( listObject )
-	.command( deleteObject )
-	.command( downloadObject )
-	.command( listBuckets )
-	.command( renameObject )
-	
-	// -Here other command
+	.command( cmd.localFolder )
+	.command( cmd.uploadObject )
+	.command( cmd.createBucket )
+	.command( cmd.listObject )
+	.command( cmd.deleteObject )
+	.command( cmd.downloadObject )
+	.command( cmd.listBuckets )
+	.command( cmd.renameObject )
 	.demandCommand()
-	.example('node myStorage.js ls my-project-inegi')
+	.example('node myStorage.js ls-o my-project-inegi')
 	.help()
 	.alias('help', 'h')
-	.config( jsonFile.config )
+	// .config( SettingsFile.config )
 	.wrap( yargs.customWrap )
 	.argv;
