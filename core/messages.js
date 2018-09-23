0c0179f7f6c925ac6c8320ddb03fdbb8303e471f
Paco Preciado Mendoza <__pm@outlook.com>
2018-09-22 20:20:48 -0500

    :sparkles: Add file that contains all messages to will be displayed in console issue: #18

---
+++ core/messages.js
@@ -0,0 +1,15 @@
+module.exports = ( code,  path )  => {
+    let messages = {   
+        PATH_CREATED : `Success: your path has been created in: ${path}`,
+        PATH_NOT_EXISTS : '\nYour path is not exits, take us a moment to create it...',
+        PATH_ALREADY_USE : `\nThis path is now in use: ${path}`,
+        FOLDER_NOT_EXISTS : "\nYou don't have any container folder, we will create one...\n",
+        FOLDER_CREATED : `Success: The folder has been created in: ${path}`,
+        FOLDER_SAME_PATH_EXIST : '\nYou already have a container folder in the same path...',
+        EXIST_FOLDER : `\nYou already have a container folder here! \nIt just has changed the path, it's now: ${path}`,
+        NO_EXIST_PATH_PARAMETER : `\nThe path is not exists:`,
+        TRY_AGAIN : '\nSorry, Try again please...',
+        CONFIG_SAVED : 'Done! You new config has been saved.',
+    }
+    return messages[code];
+}
