4e87cce818784cccd6a2cdc7f893f24e7e458584
Paco Preciado Mendoza <__pm@outlook.com>
2018-09-24 15:19:38 -0500

    :ok_hand: Rename command aliases issue: #6

--- core/Buckets/listBuckets.js
+++ core/Buckets/listBuckets.js
@@ -1,25 +1,17 @@
 const storage = require('../googleStorage');
 
 module.exports = {
-
-//quitar el parametro innecesario
-    command: 'listBuckets',
-    aliases: ['lsb', 'listB'],
-    describe: 'List all Buckets',
-    handler: argv => {
-
-        storage
-        .getBuckets()
-        .then(results => {
-          const buckets = results[0];
-      
-          console.log('Buckets:');
-          buckets.forEach(bucket => {
-            console.log(bucket.name);
-          });
-        })
-        .catch(err => {
-          console.error('ERROR:', err.errors[0].message);
-        });
-    }
+	command: 'listBuckets',
+	aliases: ['ls-b'],
+	describe: 'List all Buckets',
+	handler: argv => {
+		storage
+			.getBuckets()
+			.then(results => {
+				const buckets = results[0];
+				console.log('Buckets:');
+				buckets.forEach( bucket => console.log(bucket.name) );
+			})
+			.catch(err => console.error('ERROR:', err.errors[0].message) );
+	}
 }
